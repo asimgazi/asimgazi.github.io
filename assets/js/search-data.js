2fb232d1e73a192033ee36c8d98d29fb611abdfd
asimgazi <50971563+asimgazi@users.noreply.github.com>
2025-04-12 23:48:55 +0000

    Deploying to gh-pages from @ asimgazi/asimgazi.github.io@f3c301999540f51ab6dc828de535bbe1e0828949 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%67%61%7A%69@%73%65%61%73.%68%61%72%76%61%72%64.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
