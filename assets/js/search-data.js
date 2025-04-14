080dd5c63ca855304e0acaf97de8406a219d7051
asimgazi <50971563+asimgazi@users.noreply.github.com>
2025-04-14 04:01:37 +0000

    Deploying to gh-pages from @ asimgazi/asimgazi.github.io@972d841d25b3ecf95d944a86e8903fd58237380e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
